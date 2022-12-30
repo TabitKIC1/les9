8cf61b9c050704e3cd5630ad953ede214d32154a
TabitKIC1 <81378602+TabitKIC1@users.noreply.github.com>
2022-12-30 23:35:49 +0300

    Add files via upload

---
+++ ex1.cs
@@ -0,0 +1,18 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все 
+// натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int Num(int a)
+{
+    if(a==1) return 1;
+    System.Console.Write($"{a} ");
+    return Num(a-1);
+}
+
+System.Console.WriteLine("Введите число");
+int s = Convert.ToInt32(Console.ReadLine());
+int x = Num(s);
+
+System.Console.WriteLine(x);
---
+++ ex2.cs
@@ -0,0 +1,35 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int Sum(int a, int b, int z)
+{
+int res = z;
+if(a==b)
+{
+    return 0;
+}
+
+else
+{
+ a++;
+ res = a +Sum(a,b,z);
+ return res;   
+}
+}
+
+       
+
+
+
+System.Console.WriteLine("Введите первое число");
+int x = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите второе число");
+int f = Convert.ToInt32(Console.ReadLine());
+
+
+int summ = Sum(x,f,0);
+
+System.Console.WriteLine(summ);
